c81bbbaf03ea03249053be5527258167aa96f7da
dr-insaner <71705065+dr-insaner@users.noreply.github.com>
2021-03-05 08:51:21 +0100

    verbraucherschalten ausgelagert

--- stromzaehler.py
+++ stromzaehler.py
@@ -5,8 +5,9 @@ import socket, time, datetime, platform, os
 import csv
 import struct
 import influxdb
-import ezrdata
-import ds18b20data
+import ezrdata              #meine Funktion zum Auslesen der Fußbodenheizung
+import ds18b20data          #meine Funktion zum Heizungssensoren lesen
+import verbraucherschalten  #meine Funktion zum Verbraucher schalten
 import urllib.request
 
 
@@ -271,8 +272,8 @@ def ds18b20sensoren_to_influx(dbClient, data1):
     except Exception as e:
         print("Fehler beim speichern in influxdb (ds18b20sensoren)", e)
 
-def verbraucher_schalten(data, verbraucherstate):
-    """Funktion soll Zusatzverbraucher einschalten sobald ein gewisse Leistung eingespeist wird.
+""" def verbraucher_schalten(data, verbraucherstate):
+    Funktion soll Zusatzverbraucher einschalten sobald ein gewisse Leistung eingespeist wird.
      Ab dieser Leistung werden alle Zusatzverbraucher eingeschaltet und dann sukzessive abgeschaltet, sobald wieder Strom aus dem Netz bezogen wird.
 
     Args:
@@ -280,9 +281,39 @@ def verbraucher_schalten(data, verbraucherstate):
 
     Returns:
         [integer]: Der Rückgabewert ist die Anzahl der Zusatzverbraucher, die gerade eingeschaltet sind
-    """
+    
     print('Gesamt-Leistung: {}'.format(data))
-    if data<-150 and verbraucherstate == 0: #dec_leistung<-150 bei 0 Verbrauchern Verbraucher 1 einschalten
+    if data<-240 and verbraucherstate == 2: #dec_leistung<-150 bei 1 Verbrauchern Verbraucher 2 einschalten
+        try:
+            fp = urllib.request.urlopen("http://192.168.2.177/cm?cmnd=Power1%20ON") #Badheizung
+            mybytes = fp.read()
+            mystring = mybytes.decode("utf8")
+            if mystring == '{"POWER":"ON"}':
+                verbraucherstate = 3
+            else:
+                verbraucherstate = 13
+            fp.close()
+            print('Verbraucher Badheizung ein: {}'.format(verbraucherstate))
+        except OSError as err:
+            print("OS error: {0}".format(err))
+            mystring = '{"POWER":""}'
+            verbraucherstate = 13
+    if data<-100 and verbraucherstate == 1: #dec_leistung<-150 bei 1 Verbrauchern Verbraucher 2 einschalten
+        try:
+            fp = urllib.request.urlopen("http://192.168.2.176/cm?cmnd=Power1%20ON") #Speisekammer
+            mybytes = fp.read()
+            mystring = mybytes.decode("utf8")
+            if mystring == '{"POWER":"ON"}':
+                verbraucherstate = 2
+            else:
+                verbraucherstate = 12
+            fp.close()
+            print('Verbraucher Speisekammer ein: {}'.format(verbraucherstate))
+        except OSError as err:
+            print("OS error: {0}".format(err))
+            mystring = '{"POWER":""}'
+            verbraucherstate = 12
+    if data<-100 and verbraucherstate == 0: #dec_leistung<-150 bei 0 Verbrauchern Verbraucher 1 einschalten
         try:
             fp = urllib.request.urlopen("http://192.168.2.187/cm?cmnd=Power1%20ON") #Waschküche
             mybytes = fp.read()
@@ -290,27 +321,18 @@ def verbraucher_schalten(data, verbraucherstate):
             if mystring == '{"POWER":"ON"}':
                 verbraucherstate = 1
             else:
-                verbraucherstate = 9
+                verbraucherstate = 11
             fp.close()
+            print('Verbraucher Waschküche ein: {}'.format(verbraucherstate))
         except OSError as err:
             print("OS error: {0}".format(err))
             mystring = '{"POWER":""}'
-            verbraucherstate = 9
-    if data<-150 and verbraucherstate == 1: #dec_leistung<-150 bei 1 Verbrauchern Verbraucher 2 einschalten
-        try:
-            fp = urllib.request.urlopen("http://192.168.2.186/cm?cmnd=Power1%20ON") #Speisekammer
-            mybytes = fp.read()
-            mystring = mybytes.decode("utf8")
-            if mystring == '{"POWER":"ON"}':
-                verbraucherstate = 2
-            else:
-                verbraucherstate = 9
-            fp.close()
-        except OSError as err:
-            print("OS error: {0}".format(err))
-            mystring = '{"POWER":""}'
-            verbraucherstate = 9
-    if data>50 and verbraucherstate == 1:
+            verbraucherstate = 11
+
+
+
+#ausschalten.............................................
+    if data>50 and verbraucherstate == 1 or verbraucherstate == 11 and data>50:
         try:
             fp = urllib.request.urlopen("http://192.168.2.187/cm?cmnd=Power1%20OFF") #Waschküche
             mybytes = fp.read()
@@ -318,29 +340,45 @@ def verbraucher_schalten(data, verbraucherstate):
             if mystring == '{"POWER":"OFF"}':
                 verbraucherstate = 0
             else:
-                verbraucherstate = 9
+                verbraucherstate = 11
             fp.close()
+            print('Verbraucher 1 aus: {}'.format(verbraucherstate))
         except OSError as err:
             print("OS error: {0}".format(err))
             mystring = '{"POWER":""}'
-            verbraucherstate = 9
-    if data>50 and verbraucherstate == 2:
+            verbraucherstate = 11
+    if data>50 and verbraucherstate == 2 or verbraucherstate == 12 and data>50:
         try:
-            fp = urllib.request.urlopen("http://192.168.2.186/cm?cmnd=Power1%20OFF") #Speisekammer
+            fp = urllib.request.urlopen("http://192.168.2.176/cm?cmnd=Power1%20OFF") #Speisekammer
             mybytes = fp.read()
             mystring = mybytes.decode("utf8")
             if mystring == '{"POWER":"OFF"}':
                 verbraucherstate = 1
             else:
-                verbraucherstate = 9
+                verbraucherstate = 12
             fp.close()
+            print('Verbraucher 2 aus: {}'.format(verbraucherstate))
         except OSError as err:
             print("OS error: {0}".format(err))
             mystring = '{"POWER":""}'
-            verbraucherstate = 9
-
+            verbraucherstate = 12
+    if data>80 and verbraucherstate == 3 or verbraucherstate == 13 and data>50:
+        try:
+            fp = urllib.request.urlopen("http://192.168.2.177/cm?cmnd=Power1%20OFF") #Badheizung
+            mybytes = fp.read()
+            mystring = mybytes.decode("utf8")
+            if mystring == '{"POWER":"OFF"}':
+                verbraucherstate = 2
+            else:
+                verbraucherstate = 13
+            fp.close()
+            print('Verbraucher 3 aus: {}'.format(verbraucherstate))
+        except OSError as err:
+            print("OS error: {0}".format(err))
+            mystring = '{"POWER":""}'
+            verbraucherstate = 13
     return verbraucherstate
-
+ """
 
 if __name__ == "__main__":
     dbClient = influxdb.InfluxDBClient(
@@ -365,16 +403,15 @@ if __name__ == "__main__":
 
 
         #Verbraucher schalten
-        print('Trocknerstatus: {}'.format(verbraucher_schalten(stromz.dec_leistung, verbraucherstate)))
-        verbraucherstate = verbraucher_schalten(stromz.dec_leistung, verbraucherstate) #Funktionsaufruf
-
+        print('Trocknerstatus: {}'.format(verbraucherschalten.verbraucher_schalten(stromz.dec_leistung, verbraucherstate)))
+        verbraucherstate = verbraucherschalten.verbraucher_schalten(stromz.dec_leistung, verbraucherstate) #Funktionsaufruf
         strom_to_csv(stromz)
         strom_to_influx(dbClient, stromz, verbraucherstate)
         #print('strom2influx')
 
 
-        skip += 1
-        if skip >= 20:      #nur jeden 20ten Durchlauf die Temperaturen lesen und in DB schreiben
+        #skip += 1
+        if skip <= 0:      #erst mal deaktiviert, da heizung nicht mehr reagiert. war: nur jeden 20ten Durchlauf die Temperaturen lesen und in DB schreiben
             data = ezrdata.poll_all()
             heizung_to_influx(dbClient, data)
             print(data)
